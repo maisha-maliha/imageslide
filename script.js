4a1a70c9f95014581b23007306b000e57b988ca8
maisha-maliha <officiallymaisha@gmail.com>
2020-06-01 17:46:17 +0600

    shit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const con = document.querySelectorAll('.con')
 const btn = document.getElementsByTagName('button')
 var i = 0
 
-// btn[0].addEventListener('click', prev)
+btn[0].addEventListener('click', prev)
 btn[1].addEventListener('click', next)
 
 function next () {
@@ -12,48 +12,40 @@ function next () {
     con[i].style = 'left:0;z-index:3;opacity:1'
     con[0].style = 'opacity:0;z-index:1;left:-500px'
     con[2].style = 'opacity:0;z-index:2;left:-500px'
-    i=2
   }
-  if (i === 1) {
+  else if (i === 1) {
     console.log(i)
     i = 2
     con[i].style = 'left:0;z-index:3;opacity:1'
     con[0].style = 'opacity:0;z-index:1;left:-500px'
     con[1].style = 'opacity:0;z-index:2;left:-500px'
-    i=0
   }
-  if (i === 2) {
+  else if (i === 2) {
     console.log(i)
     i = 0
     con[i].style = 'left:0;z-index:3;opacity:1'
     con[2].style = 'opacity:0;z-index:1;left:-500px'
     con[1].style = 'opacity:0;z-index:2;left:-500px'
-    i=1
   }
 }
-// function prev () {
-//   if (i === 0) {
-//     console.log(i)
-//     i = 2
-//     con[i].style = 'left:0;z-index:3;opacity:1'
-//     con[0].style = 'opacity:0;z-index:1;left:-500px'
-//     con[1].style = 'opacity:0;z-index:2;left:-500px'
-//     i = 2
-//   }
-//   if (i === 1) {
-//     console.log(i)
-//     i = 2
-//     con[i].style = 'left:0;z-index:3;opacity:1'
-//     con[0].style = 'opacity:0;z-index:1;left:-500px'
-//     con[1].style = 'opacity:0;z-index:2;left:-500px'
-//     i = 0
-//   }
-//   if (i === 2) {
-//     console.log(i)
-//     i = 0
-//     con[i].style = 'left:0;z-index:3;opacity:1'
-//     con[2].style = 'opacity:0;z-index:1;left:-500px'
-//     con[1].style = 'opacity:0;z-index:2;left:-500px'
-//     i = 1
-//   }
-// }
+function prev () {
+  if (i === 0) {
+    i = 2
+    con[i].style = 'left:0;z-index:3;opacity:1'
+    con[0].style = 'opacity:0;z-index:1;left:-500px'
+    con[1].style = 'opacity:0;z-index:2;left:-500px'
+  }
+  else if (i === 1) {
+    i = 0
+    con[i].style = 'left:0;z-index:3;opacity:1'
+    con[2].style = 'opacity:0;z-index:1;left:-500px'
+    con[1].style = 'opacity:0;z-index:2;left:-500px'
+    i = 0
+  }
+  else if (i === 2) {
+    i = 1
+    con[i].style = 'left:0;z-index:3;opacity:1'
+    con[2].style = 'opacity:0;z-index:1;left:-500px'
+    con[0].style = 'opacity:0;z-index:2;left:-500px'
+  }
+}
